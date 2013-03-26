26c7b7bf6f5bb768b92419a67a0b87e6672ea5d8
liontercio <liontercio@hotmail.com>
2013-03-25 21:27:23 -0300

    baklal

--- src/dao/LocacaoDAO.java
+++ src/dao/LocacaoDAO.java
@@ -1,13 +1,11 @@
 package dao;
 
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Collection;
-
 import db.BancoDeDados;
-
 import entidades.Locacao;
 
 public class LocacaoDAO implements DAO<Locacao>{
@@ -19,7 +17,6 @@ public class LocacaoDAO implements DAO<Locacao>{
 		BancoDeDados.conecta();
 		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);
 		pstm.setDouble(1, obj.getValor());
-		Calendar cal = Calendar.getInstance();
 		pstm.setDate(2, obj.getDtDevolucao());
 		pstm.setDate(3, obj.getDtDevolucaoAgendada());
 		pstm.setString(4, obj.getCliente().getCpf());
@@ -31,13 +28,30 @@ public class LocacaoDAO implements DAO<Locacao>{
 
 	//alterar uma locação. Ex: adicionar multa.
 	@Override
-	public void update(Locacao obj) {
-		
+	public void update(Locacao obj) throws ClassNotFoundException, SQLException {
+		String sql = "UPDATE locacao (valor, dt_locacao, dt_devolucao_agendada, " +
+				"cpf_cliente, mat_funcionario, cod_midia) VALUES (?, ?, ?, ?, ?, ?) WHERE id = ?";
+		BancoDeDados.conecta();
+		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);
+		pstm.setDouble(1, obj.getValor());
+		pstm.setDate(2, obj.getDtDevolucao());
+		pstm.setDate(3, obj.getDtDevolucaoAgendada());
+		pstm.setString(4, obj.getCliente().getCpf());
+		pstm.setInt(5, obj.getFuncionario().getMatricula());
+		pstm.setInt(6, obj.getMidia().getId());
+		pstm.setInt(7, obj.getId());
+		pstm.execute();
+		BancoDeDados.desconectar();
 	}
 
 	@Override
-	public void remove(Locacao obj) {
-		// TODO Auto-generated method stub
+	public void remove(Locacao obj) throws ClassNotFoundException, SQLException {
+		String sql = "DELETE locacao WHERE id = ?";
+		BancoDeDados.conecta();
+		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);		
+		pstm.setInt(1, obj.getId());
+		pstm.execute();
+		BancoDeDados.desconectar();
 		
 	}
 
@@ -47,21 +61,80 @@ public class LocacaoDAO implements DAO<Locacao>{
 		return null;
 	}
 	
-	/*public Locacao get(Locacao nome) {
-		// TODO Auto-generated method stub
-		return null;
-	}*/
+	public Locacao get(int id) throws ClassNotFoundException, SQLException {
+		String sql = "SELECT * FROM locacao WHERE id = ?";
+		BancoDeDados.conecta();
+		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);;
+		pstm.setInt(1, id);
+		ResultSet res = pstm.executeQuery();
+		Locacao locacao = null;
+		while (res.next()){
+			locacao = new Locacao();
+			locacao.setCliente(Facade.getCliente(res.getInt("cpf_cliente")));
+			locacao.setFuncionario(Facade.getFuncionario(res.getInt("mat_funcionario")));
+			locacao.setMidia(Facade.getDVD(res.getInt("cod_midia")));
+			locacao.setDtDevolucao(res.getDate("dt_devolucao"));
+			locacao.setDtDevolucaoAgendada(res.getDate("dt_devolucao_agendada"));
+			locacao.setDtLocacao(res.getDate("dt_devolucao_agendada"));
+			locacao.setId(res.getInt("id"));
+			locacao.setValor(res.getDouble("valor"));
+			locacao.setValorPago(res.getDouble("valor_pago"));
+		}
+		BancoDeDados.desconectar();
+		
+		return locacao;
+	}	
 
 	@Override
-	public Collection<Locacao> get() {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Locacao> get() throws ClassNotFoundException, SQLException {
+		String sql = "SELECT * FROM locacao";
+		BancoDeDados.conecta();
+		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);;
+		ResultSet res = pstm.executeQuery();
+		
+		ArrayList<Locacao> locacoes = new ArrayList<Locacao>();
+		while (res.next()){
+			Locacao locacao = new Locacao();
+			locacao.setCliente(Facade.getCliente(res.getInt("cpf_cliente")));
+			locacao.setFuncionario(Facade.getFuncionario(res.getInt("mat_funcionario")));
+			locacao.setMidia(Facade.getDVD(res.getInt("cod_midia")));
+			locacao.setDtDevolucao(res.getDate("dt_devolucao"));
+			locacao.setDtDevolucaoAgendada(res.getDate("dt_devolucao_agendada"));
+			locacao.setDtLocacao(res.getDate("dt_devolucao_agendada"));
+			locacao.setId(res.getInt("id"));
+			locacao.setValor(res.getDouble("valor"));
+			locacao.setValorPago(res.getDouble("valor_pago"));
+			locacoes.add(locacao);
+		}
+		BancoDeDados.desconectar();
+		
+		return locacoes;
 	}
 
 	@Override
-	public Collection<Locacao> get(String regex) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Locacao> get(String regex) throws ClassNotFoundException, SQLException {
+		String sql = regex;
+		BancoDeDados.conecta();
+		PreparedStatement pstm = BancoDeDados.getConexao().prepareStatement(sql);;
+		ResultSet res = pstm.executeQuery();
+		
+		ArrayList<Locacao> locacoes = new ArrayList<Locacao>();
+		while (res.next()){
+			Locacao locacao = new Locacao();
+			locacao.setCliente(Facade.getCliente(res.getInt("cpf_cliente")));
+			locacao.setFuncionario(Facade.getFuncionario(res.getInt("mat_funcionario")));
+			locacao.setMidia(Facade.getDVD(res.getInt("cod_midia")));
+			locacao.setDtDevolucao(res.getDate("dt_devolucao"));
+			locacao.setDtDevolucaoAgendada(res.getDate("dt_devolucao_agendada"));
+			locacao.setDtLocacao(res.getDate("dt_devolucao_agendada"));
+			locacao.setId(res.getInt("id"));
+			locacao.setValor(res.getDouble("valor"));
+			locacao.setValorPago(res.getDouble("valor_pago"));
+			locacoes.add(locacao);
+		}
+		BancoDeDados.desconectar();
+		
+		return locacoes;
 	}
 
 }
